13efa70aece3aa53ceaf5ce6533b71e3e905f3e9
rsaleh <raihan.saleh@gmail.com>
2021-04-09 16:15:23 -0400

    Restructured the project into phases with smaller steps inside each phase. Deleted un necessary lines of code in the html file, and updated the entire js file with new phases commented out. The js code now is restructured to have classes of objects with methods built into them.

--- scripts.js
+++ scripts.js
@@ -1,80 +1,61 @@
 const token = 'Tpk_219118a840804c0e83a9d0c7fbc7e1bc'        // need to update these for the real app
 const baseURL = 'https://sandbox.iexapis.com/stable/stock'       // need to update these for the real app
 
-let stockPick = 'GME'
-let buyingPower = 100000;
-let portfolioValue = buyingPower;
-let portfolioArr = [];
-
-
-document.getElementById('buyBtn').addEventListener('click', onClick)
-
-function onClick(e){
-  e.preventDefault()
-  console.log(historicalData)
-  testStock.marketValue = historicalData.close
-}
-
-
-
-
-//==== Constructors Go Here ====//
-
-
 class Stock {
-  constructor(ticker, shares, cost, date, currentPrice) {
-    this.ticker = ticker
-    this.shares = shares
-    this.cost = cost
-    this.purchaseDate = date
-    this.marketValue = shares * currentPrice
+  constructor(stockTicker, purchaseDate, pricePerShare, numOfShares) {
+    this.stockTicker = stockTicker
+    this.purchaseDate = purchaseDate
+    this.pricePerShare = pricePerShare
+    this.numOfShares = numOfShares
   }
 }
 
-const testStock = new Stock('gme', 10, 15.20, 'jan 15, 2021', 2)
+class Portfolio {
+  constructor(){
+    this.buyingPower = 100000
+    this.purchasedStocks = []
+    this.stockTickers = []
+    this.sharesPerStock = {}
+  }
+  addStock(purchasedStock){
+    this.purchasedStocks.push(purchasedStock)
+    this.stockTickers.push(purchasedStock.stockTicker)
+    this.sharesPerStock[purchasedStock.stockTicker] = purchasedStock.numOfShares
+  }
+  getStockTickers(){
+    return this.stockTickers
+  }
+  getSharesPerStock(stockTicker){
+    return this.sharesPerStock[stockTicker]
+  }
+}
 
+let portfolio = new Portfolio()
 
+// ===== Phase One: Get a price of a stock at a certain date ===== //
+document.getElementById('getHistoricalDayPrice').addEventListener('click', getHistorricalDayPrice)
 
-
-//==== Functions Go Here ====//
-
-
-// Buying a stock
-// function buyStock(e){
-//   e.preventDefault()
-    
+async function getHistorricalDayPrice() {
+  let stockTicker  = document.getElementById('stockTicker').value
+  let historicalDate  = document.getElementById('historicalDate').value
   
-//   let cost  // run 
+  // console.log(`stockTicker: ${stockTicker}`)
+  // console.log(`historicalDate: ${historicalDate}`)
   
+  let url = `${baseURL}/${stockTicker}/chart/3m?filter=symbol,date,close&token=${token}`
   
-//   let cost // 1. grab the price of the stock at the given year
-//   let shares // 2. grab the number of stocks to buy from input field
-//   buyingPower -= cost * shares
-  
-//   // 3. push stock to portfolio array using the constructor StockAdd
-//   // 4. update portfolioValue
-//   // this should be the sum of all the marketvalues of the portfolio array. maybe a for-loop or reduce method?
-  
-//   // 5. update portfolioChart **********
-// }
-
-// setMarketValue() {
-//   this.marketValue = getQuote(this.ticker) * shares
-//   return
-// }
-
-// let response 
-
-
-// to get the quote of the selected stock
-async function getQuote(ticker) {
-  let url = `${baseURL}/${ticker}/quote?token=${token}`
   fetch(url)
   .then(result => result.json())
-  .then(result => {
-    console.log(`${result.symbol}, ${result.latestPrice}`)
-    console.log(typeof(result.latestPrice))
-    
+  .then(dailyData => {
+    // console.log(`dailyData: ${dailyData}`)
+    for (dayData of dailyData) {
+      if (dayData.date === historicalDate) {
+        let historicalDayPrice = dayData.close
+        // console.log(`historicalDayPrice: ${historicalDayPrice}`)
+        document.getElementById('historicalDayPrice').value = historicalDayPrice
+        break
+      }
+    }
   })
   .catch(err => {
     console.log('something went wrong', err)
@@ -82,13 +63,45 @@ async function getQuote(ticker) {
 }
 
 
-// to get batch quotes 
-async function getBatchQuotes() {
-  let url = `${baseURL}/market/batch?symbols=aapl,fb,xom,gme&types=quote&range=1m&last=5&token=${token}`
+// ===== Phase Two: Purchase Stock ===== //
+document.getElementById('buyButton').addEventListener('click', makePurchase)
+
+function makePurchase(){
+  let stockTicker = document.getElementById('stockTicker').value
+  let purchaseDate = document.getElementById('historicalDate').value
+  let pricePerShare = document.getElementById('historicalDayPrice').value
+  let numOfShares = document.getElementById('sharesToBuy').value
+  let totalPurchasePrice = pricePerShare * numOfShares
+  
+  console.log(`stockTicker: ${stockTicker}`)
+  console.log(`purchaseDate: ${purchaseDate}`)
+  console.log(`pricePerShare: ${pricePerShare}`)
+  console.log(`numOfShares: ${numOfShares}`)
+  console.log(`totalPurchasePrice: ${totalPurchasePrice}`)
+  
+  let purchasedStock = new Stock(stockTicker, purchaseDate, pricePerShare, numOfShares)
+  portfolio.addStock(purchasedStock)
+}
+
+
+document.getElementById('updatePortfolioValue').addEventListener('click', updatePortfolioValue)
+
+async function updatePortfolioValue() {
+  let stockTickers = portfolio.getStockTickers()
+  let url = `${baseURL}/market/batch?symbols=${stockTickers.join()}&types=quote&token=${token}`
+  
   fetch(url)
   .then(result => result.json())
-  .then(result => {
-        
+  .then(stockQuotes => {
+    console.log(stockQuotes)
+    
+    let portfolioValue = 0
+    for (stockTicker of stockTickers) {
+      portfolioValue += stockQuotes[stockTicker]["quote"]["latestPrice"] * portfolio.getSharesPerStock(stockTicker)
+    }
+    console.log(`portfolioValue: ${portfolioValue}`)
+
+    document.getElementById('portfolioValue').innerText = portfolioValue
   })
   .catch(err => {
     console.log('something went wrong', err)
@@ -101,44 +114,22 @@ async function getBatchQuotes() {
 
 
 
-// to get the historical quote of the selected stock
-// async function getHistoricQuote (ticker, date) {
-//   let url = `${baseURL}/stock/${ticker}/quote?token=${token}`
-//   fetch(url)
-//   .then(result => result.json())
-//   .then(result => {
+
+// // Buying a stock
+// // function buyStock(e){
+// //   e.preventDefault()
     
-//   })
-//   .catch(err => {
-//     console.log('something went wrong', err)
-//   })
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-// use later
-  class Portfolio {
-    constructor(){
-      this.name = "Portfolio"
-      this.buyingPower = 100000
-      this.value = this.buyingPower
-      this.stocks = []
-    }
   
-    buyStock(stock){
-    }
-    
-    sellStock(ticker){
-      const index = this.stocks.findIndex(s => s.ticker === ticker)
-      this.stocks.splice(index,1)
-    }
-  }
+// //   let cost  // run 
+  
+  
+// //   let cost // 1. grab the price of the stock at the given date
+// //   let shares // 2. grab the number of stocks to buy from input field
+// //   buyingPower -= cost * shares
+  
+// //   // 3. push stock to portfolio array using the constructor Stock
+// //   // 4. update portfolioValue
+// //   // this should be the sum of all the marketvalues of the portfolio array. maybe a for-loop or reduce method?
+  
+// //   // 5. update portfolioChart **********
+// // }
