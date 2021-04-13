72e4e2875c3b386365b571d6c1eec455873ff78b
rsaleh <raihan.saleh@gmail.com>
2021-04-12 23:30:23 -0400

    Fixed a bunch of bugs. Does not allow to buy 0 shares, to change stock ticker mid purchase, or to buy without a date. Also added in the ability to throw different errors.

--- scripts.js
+++ scripts.js
@@ -8,6 +8,9 @@ class Stock {
     this.pricePerShare = pricePerShare
     this.numOfShares = numOfShares
   }
+  getTotalPurchasedPrice () {
+    return this.numOfShares * this.pricePerShare
+  }
 }
 
 class Portfolio {
@@ -18,6 +21,23 @@ class Portfolio {
     this.sharesPerStock = {}
   }
   addStock(purchasedStock){
+    if (purchasedStock.purchaseDate === "") {
+      throw "FAILED TO BUY \nProvide date to receive historical stock quote"
+    }
+    if (purchasedStock.numOfShares <= 0) {
+      throw "FAILED TO BUY\nMust buy at least 1 share"
+    } 
+    if (this.buyingPower < purchasedStock.getTotalPurchasedPrice()) {
+      throw `FAILED TO BUY\nNot Enough Buying Power\nNeed additional $${(purchasedStock.getTotalPurchasedPrice() - this.buyingPower).toFixed(2)}`
+    }
+    if (purchasedStock.stockTicker === "DMX") {
+      this.purchasedStocks.push(purchasedStock)
+      this.stockTickers.push(purchasedStock.stockTicker)
+      this.sharesPerStock[purchasedStock.stockTicker] = purchasedStock.numOfShares
+      throw "X GON GIVE IT TO YOU"
+    }
+
+    this.buyingPower -= purchasedStock.getTotalPurchasedPrice()
     this.purchasedStocks.push(purchasedStock)
     this.stockTickers.push(purchasedStock.stockTicker)
     this.sharesPerStock[purchasedStock.stockTicker] = purchasedStock.numOfShares
@@ -35,17 +55,25 @@ class Portfolio {
 
 let portfolio = new Portfolio()
 
-// ===== Phase One: Get a price of a stock at a certain date ===== //
-document.getElementById('getHistoricalDayPrice').addEventListener('click', getHistorricalDayPrice)
 
-async function getHistorricalDayPrice() {
-  let stockTicker  = document.getElementById('stockTicker').value
+// ===== Phase One: Get a price of a stock at a certain date ===== //
+document.getElementById('getHistoricalDayPrice').addEventListener('click', getHistoricalDayPriceClickEvent)
+
+async function getHistoricalDayPriceClickEvent(e) {
+  if (document.getElementById('stockTicker').value.toUpperCase() === "DMX") {
+    return alert('X GON GIVE IT TO YOU')
+  }
+  getHistoricalDayPrice()
+}
+
+async function getHistoricalDayPrice(callback=false) {
+  let stockTicker  = document.getElementById('stockTicker').value.toUpperCase()
   let historicalDate  = document.getElementById('historicalDate').value
+  let url = `${baseURL}/${stockTicker}/chart/3m?filter=symbol,date,close&token=${token}`
+  let historicalDayPrice
   
   console.log(`stockTicker: ${stockTicker}`)
   console.log(`historicalDate: ${historicalDate}`)
-  
-  let url = `${baseURL}/${stockTicker}/chart/3m?filter=symbol,date,close&token=${token}`
   
   fetch(url)
   .then(result => result.json())
@@ -53,11 +81,14 @@ async function getHistorricalDayPrice() {
     console.log(`dailyData: ${dailyData}`)
     for (let dayData of dailyData) {
       if (dayData.date === historicalDate) {
-        let historicalDayPrice = dayData.close
+        historicalDayPrice = dayData.close
         console.log(`historicalDayPrice: ${historicalDayPrice}`)
         document.getElementById('historicalDayPrice').value = historicalDayPrice
         break
       }
+    }
+    if (callback) {
+      callback()
     }
   })
   .catch(err => {
@@ -69,31 +100,25 @@ async function getHistorricalDayPrice() {
 // ===== Phase Two: Purchase Stock ===== //
 document.getElementById('buyButton').addEventListener('click', makePurchase)
 
-function makePurchase(){
-  let stockTicker = document.getElementById('stockTicker').value.toUpperCase()
-  let purchaseDate = document.getElementById('historicalDate').value
-  let pricePerShare = document.getElementById('historicalDayPrice').value
-  let numOfShares = document.getElementById('sharesToBuy').value
-  let totalPurchasePrice = pricePerShare * numOfShares
-  
-  console.log(`stockTicker: ${stockTicker}`)
-  console.log(`purchaseDate: ${purchaseDate}`)
-  console.log(`pricePerShare: ${pricePerShare}`)
-  console.log(`numOfShares: ${numOfShares}`)
-  console.log(`totalPurchasePrice: ${totalPurchasePrice}`)
-  
-  let purchasedStock = new Stock(stockTicker, purchaseDate, pricePerShare, numOfShares)
-
-  if (numOfShares === 0) {
-    alert (`MUST buy at least 1 share`)
+async function makePurchase(){
+  let callback = function() {
+    let stockTicker = document.getElementById('stockTicker').value.toUpperCase()
+    let purchaseDate = document.getElementById('historicalDate').value
+    let pricePerShare = document.getElementById('historicalDayPrice').value
+    let numOfShares = document.getElementById('sharesToBuy').value
+    
+    let purchasedStock = new Stock(stockTicker, purchaseDate, pricePerShare, numOfShares)
+    console.log(purchasedStock)
+    
+    try {
+      portfolio.addStock(purchasedStock)
+    }
+    catch(error) {
+      alert(error)
+    }
   }
 
-  if (totalPurchasePrice < portfolio.buyingPower) {
-    portfolio.addStock(purchasedStock)
-    portfolio.buyingPower -= totalPurchasePrice
-  } else {
-    alert(`NOT Enough Buying Power\nNeed additional ${totalPurchasePrice-portfolio.buyingPower}`)
-  }
+  await getHistoricalDayPrice(callback)
 }
 
 
@@ -116,7 +141,7 @@ async function updatePortfolioValue() {
     portfolioValue += portfolio.buyingPower
     console.log(`portfolioValue: ${portfolioValue}`)
 
-    document.getElementById('portfolioValue').innerText = portfolioValue
+    document.getElementById('portfolioValue').innerText = `$${portfolioValue.toFixed(2)}`
   })
   .catch(err => {
     console.log('something went wrong', err)
@@ -125,7 +150,6 @@ async function updatePortfolioValue() {
 
 
 // ===== Phase Four: Render Portfolio List On-Screen ===== //
-// Move this into the updatePortfolio  Function
 document.getElementById('updatePurchasedStocksList').addEventListener('click', updatePurchasedStocksList)
 
 function updatePurchasedStocksList(){
@@ -138,3 +162,6 @@ function updatePurchasedStocksList(){
     ulTag.appendChild(newLi)
   }
 }
+
+
+// ===== Phase Five: Create the Portfolio Chart ===== //
